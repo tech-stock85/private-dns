28bc1d7e8f4e42d26f1eb2fc6421bb2cc8aa9a85
tech-stock85 <a-ostroumova-42111278@rambler.ru>
2025-12-04 09:47:57 +0800

    add decorator.java, statistics-simple-bridge.md, app-dist.go

---
+++ app-dist.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\app-dist.go
+// Generated at: 2025-10-05T15:17:00.611Z
---
+++ decorator.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\decorator.java
+// Generated at: 2025-10-05T15:17:00.610Z
